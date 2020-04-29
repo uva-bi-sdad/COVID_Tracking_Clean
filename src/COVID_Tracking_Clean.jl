50d88d56a6550f988ce39b72c5789f42fe9f7f03
José Bayoán Santiago Calderón <jbs3hp@virginia.edu>
2020-04-28 23:07:12 -0400

    Upstream updates
    - Update to reflect current schema
    - Move more logic to Postgres

--- src/COVID_Tracking_Clean.jl
+++ src/COVID_Tracking_Clean.jl
@@ -37,7 +37,7 @@ import Base: show, summary
 # Constants
 const COVID_TRACKING_DT = DateFormat("y/m/d HH:MM Z")
 
-for (root, dirs, files) in walkdir(joinpath(@__DIR__))
+for (root, dirs, files) in walkdir(joinpath(@__DIR__, "src"))
     for file in files
         isequal("COVID_Tracking_Clean.jl", file) || include(joinpath(root, file))
     end
@@ -50,61 +50,146 @@ Performs the action to refresh the data.
 Latest data will be written to data/latest.tsv
 """
 function main(deploy::Bool = true)
-    ENV["POSTGRES_HOST"] = get(ENV, "POSTGRES_HOST", "localhost")
-    ENV["POSTGRES_PORT"] = get(ENV, "POSTGRES_PORT", "5432")
+    ENV["PGHOST"] = get(ENV, "PGHOST", "localhost")
+    ENV["PGPORT"] = get(ENV, "PGPORT", "5432")
     ENV["PAT"] = get(ENV, "PAT", "")
 
     opt = Opt("Nosferican",
               ENV["PAT"],
-              host = ENV["POSTGRES_HOST"],
-              port = parse(Int, ENV["POSTGRES_PORT"]))
+              host = ENV["PGHOST"],
+              port = parse(Int, ENV["PGPORT"]))
     execute(opt.conn,
-    """
-    CREATE EXTENSION IF NOT EXISTS btree_gist;
-    CREATE TABLE IF NOT EXISTS daily (
-        state char(2) NOT NULL,
-        checkts timestamp NOT NULL,
-        positive int,
-        negative int,
-        pending int,
-        hospitalized int,
-        death int,
-        EXCLUDE USING gist (state WITH =, checkts WITH =)
-    );
-    CREATE TABLE IF NOT EXISTS qc (
-        state char(2) NOT NULL,
-        during tsrange NOT NULL,
-        grade char(1) NOT NULL,
-        EXCLUDE USING gist (state WITH =, during WITH &&)
-    );
-    """
-    )
+            """
+            CREATE EXTENSION IF NOT EXISTS btree_gist;
+            DROP TABLE IF EXISTS daily CASCADE;
+            CREATE TABLE IF NOT EXISTS daily (
+              state char(2) NOT NULL,
+              checkts timestamp NOT NULL,
+              positive int,
+              negative int,
+              pending int,
+              hospitalized_currently int,
+              hospitalized_cumulative int,
+              icu_currently int,
+              icu_cumulative int,
+              ventilation_currently int,
+              ventilation_cumulative int,
+              recovered int,
+              death int,
+              EXCLUDE USING gist (state WITH =, checkts WITH =)
+            );
+            DROP TABLE IF EXISTS qc CASCADE;
+            CREATE TABLE IF NOT EXISTS qc (
+              state char(2) NOT NULL,
+              during timestamp NOT NULL,
+              data_quality_grade varchar(2) NOT NULL,
+              as_of timestamp timestamp NOT NULL
+            );
+            """
+            )
     shas = find_shas(opt.pat)
     qc = vcat((get_tbl(opt.pat, sha) for sha in shas)..., cols = :union)
-    qc = by(qc[:,[:state, :checkTimeEt, :grade]], :state, prune)
     qc[!,:checkTimeEt] = astimezone.(qc.checkTimeEt, utc_tz)
-    rename!(qc, :checkTimeEt => :checkts)
-    qc[!,:checkts] = replace.(string.(qc.checkts), " .." => ",")
-    sort!(qc, [:state, :checkts, :grade])
+
+    # vscodedisplay(data)
 
     daily = states_daily()
 
     execute(opt.conn, "BEGIN;")
-    load!(daily, opt.conn, "INSERT INTO daily VALUES($(join(("\$$i" for i in 1:7), ',')));")
+    load!(daily, opt.conn, "INSERT INTO daily VALUES($(join(("\$$i" for i in 1:13), ',')));")
     execute(opt.conn, "COMMIT;")
 
     execute(opt.conn, "BEGIN;")
-    load!(qc[:,[:state,:checkts,:grade]], opt.conn, "INSERT INTO qc VALUES($(join(("\$$i" for i in 1:3), ',')));")
+    load!(qc, opt.conn, "INSERT INTO qc VALUES($(join(("\$$i" for i in 1:3), ',')));")
     execute(opt.conn, "COMMIT;")
 
-    output = execute(opt.conn,
-                     """
-                     SELECT A.*, B.grade
-                        FROM daily A
-                     LEFT JOIN qc B
-                        ON A.state = B.state AND B.during @> A.checkts
-                     ORDER BY state ASC, checkts ASC;
-                     """) |>
+    output = execute(
+        opt.conn,
+        """
+        DROP SEQUENCE IF EXISTS serial CASCADE;
+        CREATE TEMP SEQUENCE serial START WITH 1;
+        SELECT
+          nextval('serial');
+        CREATE MATERIALIZED VIEW IF NOT EXISTS data_quality_grades AS (
+            WITH A AS (
+              SELECT
+                DISTINCT ON (state, during) state,
+                during,
+                data_quality_grade
+              FROM qc
+              ORDER BY
+                state ASC,
+                during ASC
+            ),
+            B AS (
+              SELECT
+                state,
+                during,
+                data_quality_grade,
+                LAG(data_quality_grade) OVER (
+                  PARTITION BY state
+                  ORDER BY
+                    during ASC
+                ) AS prev
+              FROM A
+            ),
+            C AS (
+              SELECT
+                state,
+                during,
+                data_quality_grade,
+                CASE
+                  WHEN prev IS null
+                  OR prev <> data_quality_grade THEN nextval('serial')
+                  ELSE currval('serial')
+                END AS grp
+              FROM B
+            ),
+            D AS (
+              SELECT
+                state,
+                MIN(during) AS start_ts,
+                MAX(during) AS end_ts,
+                data_quality_grade
+              FROM C
+              GROUP BY
+                state,
+                data_quality_grade,
+                grp
+            ),
+            E AS (
+              SELECT
+                *,
+                LEAD(start_ts) OVER (
+                  PARTITION BY state
+                  ORDER BY
+                    start_ts
+                ) AS until_ts
+              FROM D
+            )
+            SELECT
+              state,
+              CASE
+                WHEN until_ts IS NOT NULL THEN tsrange(start_ts, until_ts)
+                WHEN start_ts = end_ts THEN tsrange(start_ts, date_trunc('second', NOW() :: timestamp))
+                ELSE tsrange(start_ts, end_ts)
+              END AS during,
+              data_quality_grade
+            FROM E
+            ORDER BY
+              state ASC,
+              during ASC
+          );
+        SELECT
+          A.*,
+          B.data_quality_grade
+        FROM public.daily A
+        LEFT JOIN public.data_quality_grades B ON A.state = B.state
+          AND B.during @ > A.checkts
+        ORDER BY
+          state ASC,
+          checkts ASC;
+        """) |>
         rowtable
     content = base64encode(take!(CSV.write(IOBuffer(), output, delim = '\t')));
     if deploy
